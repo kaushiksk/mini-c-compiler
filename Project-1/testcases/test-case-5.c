87c25233fa0a6536fb1c637baa77015c39cec2a3
Kaushik <kaushikskalmady@gmail.com>
2018-01-17 16:06:51 +0530

    Added long and unsigned keywords

--- Project-1/testcases/test-case-5.c
+++ Project-1/testcases/test-case-5.c
@@ -10,7 +10,7 @@ Identifying tokens and displaying symbol and constants table
 Following tokens must be detected
  - Keywords   (int, long int, long long int, main include)
  - Identifiers (main,total,x,y,printf),
- - Constants  (-10, 20, 0x0f)
+ - Constants  (-10, 20, 0x0f, 123456l)
  - Strings ("Total = %d \n")
  - Special symbols and Brackets ( (), {}, ;, ,)
  - Operators (+,-,=,*,/,%,--,++)
@@ -25,7 +25,8 @@ int main()
   int x, y;
   long long int total, diff;
   int *ptr;
-  int a = 0x0f;
+  unsigned int a = 0x0f;
+  long int mylong = 123456l;
   long int i, j;
   for(i=0; i < 10; i++){
     for(j=10; j > 0; j--){
