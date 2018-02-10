ef93bc7fa2eb4250d1c2444eb4c0608886e931b0
kaushiksk <kaushikskalmady@gmail.com>
2018-02-10 15:49:39 +0530

    Added parser code

---
+++ Project-2/testcases/test-case-2.c
@@ -0,0 +1,31 @@
+/*
+Karthik M - 15CO221
+Kaushik S Kalmady - 15CO222
+Compiler Design Project 2
+
+Test Case 2
+ - Test for unmatched parantheses
+
+The output in lex/yacc should print an error message when parantheses are not matched
+
+*/
+
+#include<stdio.h>
+
+void main()
+{
+	int array1[10];
+	int array2[20;
+	
+	for(int a = 3; a<4; a++
+	{
+		array1[0]=a;
+	}
+	
+	func();
+}
+
+int func()
+{
+	return 20;
+	
