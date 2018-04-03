a59ea9cb2ee1e3e4f4700fb547ec55c2c2b98f19
kaushiksk <kaushikskalmady@gmail.com>
2018-04-03 22:00:17 +0530

    Updated testcases

---
+++ Project-4/test-func.c
@@ -0,0 +1,15 @@
+void fun2(char* s)
+{
+
+}
+
+void fun(int a, int b, int c)
+{
+  fun2("What else can I print?");
+}
+
+int main(){
+    int c;
+    c = 5;
+    fun(c, 2 , 5);
+}
--- Project-4/test.c
+++ Project-4/test.c
@@ -1,13 +1,3 @@
-void fun2(char* s)
-{
-
-}
-
-void fun(int a, int b, int c)
-{
-  fun2("What else can I print?");
-}
-
 int main(int argc)
 {
   int i;
@@ -27,15 +17,16 @@ int main(int argc)
       int arr[10];
       j = arr[7];
 
-      if(2<4)
-      break;
-      else if(2>3)
-      continue;
-      else
-      i = 4 + 5;
+      if(j < 5 && i == 3){
+        j = 2;
+        if(2<4)
+        break;
+        else if(2>3)
+        continue;
+        else
+        i = 4 + 5;
+      }
 
     }
   }
-
-  fun(i-j,2 + 3,i+j);
 }
